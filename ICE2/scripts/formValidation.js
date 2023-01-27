92fc1b7a6ab8434fdeb0aaaec0a6c0bf814303cb
Bei Jia <bei.jia@dcmail.ca>
2023-01-27 00:08:57 -0500

    complete

--- ICE2/scripts/formValidation.js
+++ ICE2/scripts/formValidation.js
@@ -1,49 +1,105 @@
-console.log("formValidation.js loaded");
+//console.log("formValidation.js loaded");
 
 /**
  * Validate the email address
  * @param {string} email    the email address to validate
  * @returns {boolean}        to indicate if the email is valid
  */
-function ValidateEmailAddressSimple(emailString) {
-    console.log('in ValidateEmailAddress');
+// function ValidateEmailAddressSimple(emailString) {
+//     console.log('in ValidateEmailAddress');
 
-    // check for @ sign
-    let atSymbol = emailString.indexOf('@');
-    if(atSymbol < 1) return false;
+//     // check for @ sign
+//     let atSymbol = emailString.indexOf('@');
+//     if (atSymbol < 1) return false;
 
-    let dot = emailString.indexOf('.');
-    if(dot <= atSymbol + 2) return false;
+//     let dot = emailString.indexOf('.');
+//     if (dot <= atSymbol + 2) return false;
 
-    // check that the dot is not at the end
-    if (dot === emailString.length - 1) return false;
+//     // check that the dot is not at the end
+//     if (dot === emailString.length - 1) return false;
 
-    return true;
-}
+//     return true;
+// }
 
 /*
  * Validate the email address
 * @param {string} emailString
 * @returns {boolean}    validation result
 */
-function ValidateEmailAddressRegex(emailString) {
-    //the regular expression to validate the email address for string+string|number bewten 2-20 characters
-    // note the / and / at the beginning and end of the expression
-    var emailRegex = /^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$/;
+// function ValidateEmailAddressRegex(emailString) {
+//     //the regular expression to validate the email address for string+string|number bewten 2-20 characters
+//     // note the / and / at the beginning and end of the expression
+//     let emailRegex = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/;
 
-    //return true if the email address is valid - how to use regex
-	return !!emailString && typeof emailString === 'string'
-		&& emailString.match(emailRegex);
+//     //return true if the email address is valid - how to use regex
+//     return !!emailString && typeof emailString === 'string'
+//         && emailString.match(emailRegex);
+// }
+
+// the function to verify that the user's name is valid
+function validateUserName() {
+    console.log('in validateUserName');
+    let userName = document.getElementById('usernameInput').value;
+    if (userName.length < 4 || userName.length > 20) {
+
+        document.getElementById('usernameError').innerHTML = 'Names should be between 4 and 20 characters long';
+        document.getElementById('usernameError').style.color = 'red';
+        return false;
+    }
+    else {
+        document.getElementById('usernameError').innerHTML = '';
+        return true;
+
+    }
+
+}
+
+// the function to clear the error messages
+function clearErrorMessages() {
+    let errorMessage = document.getElementsByClassName('errorMessage');
+    for (let i = 0; i < errorMessage.length; i++) {
+        errorMessage[i].innerHTML = "";
+        errorMessage[i].style.color = "";
+    }
+
 }
 
 
 //TODO:
-// Make all fields required
-// Add a pattern attribute in the telephone element (HTML)
-// Ensure the password is redacted into dots on screen when typed in
-// Create a function that knows if the username is valid (feel free to grab some RegEx from the Interwebs - cite source and test it!)
-// If it is not valid, be sure to update the #generalError <p></p> with a good error message and some highlighting (red background, maybe?)
-// Validate the username when the form is submitted
-// Clear any additional error message and highlighting when the form is reset
+// Make all fields required - DONE
+// Add a pattern attribute in the telephone element (HTML) - Done
+// Ensure the password is redacted into dots on screen when typed in - DONE
+// Create a function that knows if the username is valid (feel free to grab some RegEx from the Interwebs - 
+// cite source and test it!)- Done
+// If it is not valid, be sure to update the #generalError <p></p> with a good error message and some highlighting (red background, maybe?) - DONE
+// Validate the username when the form is submitted - DONE
+
+// Clear any additional error message and highlighting when the form is reset - DONE
 // COMMENT EVERYTHING :D
 // Pop your .js file into the meeting chat when you're done and we'll have a look at some examples! (Feel free to leave your name off)
+
+/*
+ * Validate the username
+* @param {string} emailString
+*/
+let submittButton = document.getElementById('submit-reg-form');
+if (submittButton) {
+    submittButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        validateUserName()
+    }, false);
+
+};
+
+/*
+ * click the reset button, the error message is cleared
+* @param {string} emailString
+*/
+let resetButton = document.getElementById('reset-reg-form');
+if (resetButton) {
+    resetButton.addEventListener('click', function (e) {
+
+        clearErrorMessages();
+    });
+
+};
