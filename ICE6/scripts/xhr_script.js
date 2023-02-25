7e65b4e82dab6d1d7f0252081d4db5a0d15998bf
Bei Jia <bei.jia@dcmail.ca>
2023-02-24 23:09:19 -0500

    ice 6 update

---
+++ ICE6/scripts/xhr_script.js
@@ -0,0 +1,32 @@
+console.log("xhr_script.js loaded");
+
+// get the button for xhr
+let btnXhr=$('#xhrJoke');
+// create a url variable
+let url_xhr='https://icanhazdadjoke.com/'
+
+// create a click callback that handles the API call
+$(btnXhr).click(()=>{
+    // instantiate an XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+    // confiture with open()
+    xhr.open("GET", url_xhr);
+    // set the necessary headers for icanhazdadjoke.com
+    xhr.setRequestHeader("Accept", "application/json");
+    // create the callback to handle the onreadystatechange
+    xhr.onreadystatechange =function(){
+        // only execute when done and status is 200
+        if (xhr.readyState==4 & xhr.status==200){
+            // get the responseText
+            let joke = xhr.responseText;
+            // console log
+            console.log(joke);
+            // JSON.parse the joke
+            let JSObjJoke=JSON.parse(joke);
+            // set the output
+            $("#output").text(JSObjJoke['joke']);
+    // send the request
+        }
+    }
+    xhr.send();
+})
